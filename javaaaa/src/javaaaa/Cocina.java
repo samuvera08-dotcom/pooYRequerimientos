930cfade079000536dea4bf342a4963025b54f03
samuvera08-dotcom <samuvera08@gmail.com>
2026-08-27 12:05:01 -0500

    Creo dos clases derivadas de la Super clase Cocina y Sala

---
+++ javaaaa/src/javaaaa/Cocina.java
@@ -0,0 +1,31 @@
+package javaaaa;
+
+public class Cocina {
+    package javaaaa;
+
+    /**
+     *
+     * @author samuelvera
+     */
+    public class Cocina extends AreaComun {
+
+        private int numeroElectrodomesticos;
+
+        public Cocina() {
+            super();
+        }
+
+        public Cocina(String nombre, int capacidadPersonas, int numeroElectrodomesticos) {
+            super(nombre, capacidadPersonas);
+            this.numeroElectrodomesticos = numeroElectrodomesticos;
+        }
+
+        public int getNumeroElectrodomesticos() {
+            return numeroElectrodomesticos;
+        }
+
+        public void setNumeroElectrodomesticos(int numeroElectrodomesticos) {
+            this.numeroElectrodomesticos = numeroElectrodomesticos;
+        }
+    }
+}
---
+++ javaaaa/src/javaaaa/Sala.java
@@ -0,0 +1,31 @@
+package javaaaa;
+
+public class Sala {
+   package javaaaa;
+
+    /**
+     *
+     * @author samuelvera
+     */
+    public class Sala extends AreaComun {
+
+        private boolean tieneTelevisor;
+
+        public Sala() {
+            super();
+        }
+
+        public Sala(String nombre, int capacidadPersonas, boolean tieneTelevisor) {
+            super(nombre, capacidadPersonas);
+            this.tieneTelevisor = tieneTelevisor;
+        }
+
+        public boolean isTieneTelevisor() {
+            return tieneTelevisor;
+        }
+
+        public void setTieneTelevisor(boolean tieneTelevisor) {
+            this.tieneTelevisor = tieneTelevisor;
+        }
+    }
+}
