151f56a8235b996796d28b527b8f58055790c817
samuvera08-dotcom <samuvera08@gmail.com>
2026-08-27 12:24:44 -0500

    Creo la ultima super clase EspacioExterior

---
+++ javaaaa/src/javaaaa/EspacioExterior.java
@@ -0,0 +1,48 @@
+package javaaaa;
+
+public class EspacioExterior {
+package javaaaa;
+
+    /**
+     *
+     * @author samuelvera
+     */
+    public class EspacioExterior {
+
+        private String nombre;
+        private boolean techado;
+
+        public EspacioExterior() {
+        }
+
+        public EspacioExterior(String nombre, boolean techado) {
+            this.nombre = nombre;
+            this.techado = techado;
+        }
+
+        public String getNombre() {
+            return nombre;
+        }
+
+        public void setNombre(String nombre) {
+            this.nombre = nombre;
+        }
+
+        public boolean isTechado() {
+            return techado;
+        }
+
+        public void setTechado(boolean techado) {
+            this.techado = techado;
+        }
+
+        public void describir() {
+            System.out.println("Espacio exterior: " + nombre + ", techado: " + (techado ? "sí" : "no"));
+        }
+    }
+
+
+
+
+
+}
