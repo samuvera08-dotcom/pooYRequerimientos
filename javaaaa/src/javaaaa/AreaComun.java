a44c6d3eef9aaf451613ac7a97c33c727f3bbb59
samuvera08-dotcom <samuvera08@gmail.com>
2026-08-27 12:01:54 -0500

    Creo una super clase AreaComun

---
+++ javaaaa/src/javaaaa/AreaComun.java
@@ -0,0 +1,45 @@
+package javaaaa;
+
+public class AreaComun {
+    package javaaaa;
+
+    /**
+     *
+     * @author samuelvera
+     */
+    public class AreaComun {
+
+        private String nombre;
+        private int capacidadPersonas;
+
+        public AreaComun() {
+        }
+
+        public AreaComun(String nombre, int capacidadPersonas) {
+            this.nombre = nombre;
+            this.capacidadPersonas = capacidadPersonas;
+        }
+
+        public String getNombre() {
+            return nombre;
+        }
+
+        public void setNombre(String nombre) {
+            this.nombre = nombre;
+        }
+
+        public int getCapacidadPersonas() {
+            return capacidadPersonas;
+        }
+
+        public void setCapacidadPersonas(int capacidadPersonas) {
+            this.capacidadPersonas = capacidadPersonas;
+        }
+
+        public void describir() {
+            System.out.println("Área común: " + nombre + ", capacidad: " + capacidadPersonas + " personas");
+        }
+    }
+
+
+}
